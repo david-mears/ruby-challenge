08784e6bca8393230bf5d0e2567ba81b0d9d3581
David Mears <kieukmail@gmail.com>
2019-10-09 10:37:03 +0100

    Implement code for original feature test - can search file for lines containing a string

--- part3/grep/spec/grep_feature_spec.rb
+++ part3/grep/spec/grep_feature_spec.rb
@@ -1,19 +1,15 @@
 describe "#my_grep" do
     CLI_COMMAND = "./lib/my_grep"
 
-    it 'can read a file and print first line' do
-        expect{ system CLI_COMMAND + " file_for_test.txt"}
-            .to output("this is a test file\n")
-            .to_stdout_from_any_process
-    end
-
     it 'searches files for a string' do
         expect{ system CLI_COMMAND + " 'giraffe' file_for_test.txt" }
-            .to output("giraffe\ni like giraffes\ndid i mention that i like giraffes? good")
+            .to output(
+                "giraffe\ni like giraffes\ndid i mention that i like giraffes? good\n"
+                )
             .to_stdout_from_any_process
     end
 
-    it 'searches files for a regular expression' do
+    xit 'searches files for a regular expression' do
         raise "TODO"
     end
 end
